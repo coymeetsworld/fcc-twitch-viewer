b70f8c5b31bff5144b79b5802d54a2d57a3fa2c5
Coy Sanders <coymeetsworld@gmail.com>
2016-07-03 02:21:02 -0700

    Removing bootstrap, working on revamping design. Adding preview image to live streaming.

--- twitch.js
+++ twitch.js
@@ -2,14 +2,23 @@ $(document).ready(function() {
 
   const NO_USER_ICON_URL= "http://www.brandsoftheworld.com/sites/default/files/styles/logo-thumbnail/public/022015/twitch.png?itok=kKyIL2PQ";
 
-  function createImage(src) {
-    var img_tag = $('<img>');
+  function createUserImage(src) {
+    var imageTag = $('<img>');
+    imageTag.addClass('userIcon');
+
     if (src != null) {
-      img_tag.attr('src', src);
+      imageTag.attr('src', src);
     } else {
-      img_tag.attr('src', NO_USER_ICON_URL);
+      imageTag.attr('src', NO_USER_ICON_URL);
     }
-    return img_tag;
+    return imageTag;
+  }
+
+  /* DRY */
+  function createPreviewImage(src) {
+    var imageTag = $('<img>');
+    imageTag.attr('src',src);
+    return imageTag;
   }
 
   function createLink(streamer_name) {
@@ -36,53 +45,84 @@ $(document).ready(function() {
 
   function getUsers(user) {
 
-    $.getJSON('https://api.twitch.tv/kraken/users/' + user + '?callback=?', function (data) {
-      console.log(data);
-      var channel_row = $('<div>');
-      channel_row.attr('class', 'row channel');
+    $.getJSON('https://api.twitch.tv/kraken/users/' + user + '?callback=?', function (userData) {
+
+      console.log("User data");
+      console.log("-------------------------");
+      console.log(userData);
+      console.log("-------------------------");
+      console.log("End user data");
+
+      /*
+        What this contains:
+        bio: "Wizards of the Coast's official live streaming channel for Magic: The Gathering tournaments and convention coverage."
+        display_name: Magic
+        logo: logo url
+      */
+      var channelItem = $('<div>');
+      channelItem.attr('class', 'flex-item');
+
       var icon_column = $('<div>');
       icon_column.attr('class', 'col-md-1');
 
-      $(createImage(data.logo)).appendTo(icon_column);
-      $(icon_column).appendTo(channel_row);
+      $(createUserImage(userData.logo)).appendTo(icon_column);
+      $(icon_column).appendTo(channelItem);
 
       var name_column = $('<div>');
       name_column.attr('class', 'col-md-3 twitch_link_column');
 
-      $(createLink(data.display_name)).appendTo(name_column);
-      $(name_column).appendTo(channel_row);
+      $(createLink(userData.display_name)).appendTo(name_column);
+      $(name_column).appendTo(channelItem);
 
-      $.getJSON('https://api.twitch.tv/kraken/streams/' + data.name + '?callback=?', function(data) {
+      $.getJSON('https://api.twitch.tv/kraken/streams/' + userData.name + '?callback=?', function(streamData) {
 
+        console.log("Stream data");
+        console.log("-------------------------");
+        console.log(streamData);
+        console.log("-------------------------");
+        console.log("Stream data");
+        /* What it contains
+            stream.preview.large;
+            stream.channel.created_at: Time when stream started(Can list how long its been going?)
+            stream.channel.game: "Name of game (i.e. Starcraft II, Poker)"
+            stream.channel.status "Best of Live at the Bike! "
+        */
         var desc_column = $('<div>');
         desc_column.attr('class', 'col-md-8 twitch_status');
 
-        if (data.status == 422) {
-          channel_row.addClass('channel_offline');
+        if (streamData.status == 422) {
+          channelItem.addClass('channel_offline');
           desc_column.text('Account closed');
-          $(desc_column).appendTo(channel_row);
-          $(channel_row).appendTo("#channels");
+          $(desc_column).appendTo(channelItem);
+          $(channelItem).appendTo("#channels");
         }
-        else if (data.stream == null) {
-          channel_row.addClass('channel_offline');
+        else if (streamData.stream == null) {
+          channelItem.addClass('channel_offline');
           desc_column.text('Offline');
-          $(desc_column).appendTo(channel_row);
-          $(channel_row).appendTo("#channels");
+          $(desc_column).appendTo(channelItem);
+          $(channelItem).appendTo("#channels");
         } else {
-          channel_row.addClass('channel_online');
-          desc_column.text(data.stream.channel.game + ": " + data.stream.channel.status);
-          $(desc_column).appendTo(channel_row);
-          $(channel_row).prependTo("#channels");
+          channelItem.addClass('channel_online');
+          desc_column.text(streamData.stream.channel.game + ": " + streamData.stream.channel.status);
+          $(desc_column).appendTo(channelItem);
+
+          var previewRow = $('<div>');
+          $(createPreviewImage(streamData.stream.preview.large)).appendTo(previewRow);
+          previewRow.addClass('previewRow');
+          $(previewRow).appendTo(channelItem);
+          $(channelItem).prependTo("#channels");
         }
 
-        //$(channel_row).appendTo("#channels");
+        //$(channelItem).appendTo("#channels");
       });
     });
 
   }
 
 
-  var usernames = ["freecodecamp", "magic", "celinalin", "nanonoko","wsopreplaystream","jonathanlittle", "liveatthebike", "esl_sc2", "ogamingsc2", "habathcx", "terakilobyte", "thomasballinger", "comster404", "brunofin"];
+  //var usernames = ["freecodecamp", "magic", "celinalin", "nanonoko","wsopreplaystream","jonathanlittle", "liveatthebike", "esl_sc2", "ogamingsc2", "habathcx", "terakilobyte", "thomasballinger", "comster404", "brunofin"];
+  var usernames = ["liveatthebike", "ogamingsc2", "magic", "freecodecamp"];
+
   //var usernames = ["coymeetsworld", "brunofin"];
   for (var i = 0; i < usernames.length; i++) {
     getUsers(usernames[i]);
