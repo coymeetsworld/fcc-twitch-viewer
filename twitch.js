03f4f29df34f239363d4300e9fc375b7f113703b
Coy Sanders <coymeetsworld@gmail.com>
2016-07-05 02:24:40 -0700

    Added Bootstrap tooltips to show info on Twitch users, if available. Also, made preview image a link to the stream and displayed number of current viewers watching.

--- twitch.js
+++ twitch.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-  //const NO_USER_ICON_URL= "http://www.brandsoftheworld.com/sites/default/files/styles/logo-thumbnail/public/022015/twitch.png?itok=kKyIL2PQ";
   const NO_USER_ICON_URL= "https://dl.dropboxusercontent.com/u/26748984/web-project-resources/freecodecamp/TwitchViewer/twitchDefaultIcon.png";
   function createUserImage(src) {
     var imageTag = $('<img>');
@@ -15,17 +14,21 @@ $(document).ready(function() {
   }
 
   /* DRY */
-  function createPreviewImage(src) {
+  function createPreviewImage(streamerName, src) {
+    var link = $('<a>');
+    link.attr('href', 'http://www.twitch.tv/' + streamerName);
+    link.attr('target', '_blank');
     var imageTag = $('<img>');
-    imageTag.attr('src',src);
-    return imageTag;
+    imageTag.attr('src', src);
+    link.html(imageTag);
+    return link;
   }
 
-  function createLink(streamer_name) {
+  function createLink(streamerName) {
     var link = $('<a>');
-    link.attr('href', 'http://www.twitch.tv/' + streamer_name);
+    link.attr('href', 'http://www.twitch.tv/' + streamerName);
     link.attr('target', '_blank');
-    link.html(streamer_name);
+    link.html(streamerName);
     return link;
   }
 
@@ -52,18 +55,18 @@ $(document).ready(function() {
 
     $.getJSON('https://api.twitch.tv/kraken/users/' + user + '?callback=?', function (userData) {
 
+      if (userData.status == 404) {
+        console.log("404 " + userData.error + ": " + userData.message);
+        return;
+      }
+
       console.log("User data");
       console.log("-------------------------");
       console.log(userData);
       console.log("-------------------------");
       console.log("End user data");
 
-      /*
-        What this contains:
-        bio: "Wizards of the Coast's official live streaming channel for Magic: The Gathering tournaments and convention coverage."
-        display_name: Magic
-        logo: logo url
-      */
+
       var channelItem = $('<li>');
       channelItem.attr('class', 'flex-item');
 
@@ -75,6 +78,17 @@ $(document).ready(function() {
       var nameColumn = $('<div>');
       nameColumn.attr('class', 'streamerName');
       $(createLink(userData.display_name)).appendTo(nameColumn);
+
+      if (userData.bio != null && userData.bio != '') {
+        console.log("Username: " + userData.name);
+        console.log("Bio: '" + userData.bio + "'");
+        var imageTag = $('<img>');
+        imageTag.attr('src', 'https://dl.dropboxusercontent.com/u/26748984/web-project-resources/freecodecamp/TwitchViewer/info.png');
+        imageTag.attr('data-toggle', 'tooltip');
+        imageTag.attr('title',userData.bio);
+        imageTag.appendTo(nameColumn);
+
+      }
       $(nameColumn).appendTo(channelItem);
 
       $.getJSON('https://api.twitch.tv/kraken/streams/' + userData.name + '?callback=?', function(streamData) {
@@ -84,23 +98,16 @@ $(document).ready(function() {
         console.log(streamData);
         console.log("-------------------------");
         console.log("Stream data");
-        /* What it contains
-            stream.preview.large;
-            stream.channel.created_at: Time when stream started(Can list how long its been going?)
-            stream.channel.game: "Name of game (i.e. Starcraft II, Poker)"
-            stream.channel.status "Best of Live at the Bike! "
-        */
+
         var statusColumn = $('<div>');
         statusColumn.attr('class', 'streamStatus');
 
         if (streamData.status == 422) {
-          /* TODO */
           channelItem.addClass('channelClosed');
           statusColumn.text('Account closed');
           $(statusColumn).appendTo(channelItem);
         }
         else if (streamData.stream == null) {
-          /* TODO */
           channelItem.addClass('channelOffline');
           statusColumn.text('Offline');
           $(statusColumn).appendTo(channelItem);
@@ -109,25 +116,28 @@ $(document).ready(function() {
           statusColumn.text(streamData.stream.channel.game + ": " + streamData.stream.channel.status);
           $(statusColumn).appendTo(channelItem);
           var streamPreview = $('<div>');
-          $(createPreviewImage(streamData.stream.preview.large)).appendTo(streamPreview);
+          $(createPreviewImage(userData.name, streamData.stream.preview.large)).appendTo(streamPreview);
           streamPreview.addClass('streamPreview');
           $(streamPreview).appendTo(channelItem);
-
           var streamPreviewInfo = $('<div>');
           streamPreviewInfo.addClass('streamPreviewInfo');
           streamPreviewInfo.html("Viewers: " + streamData.stream.viewers);
           $(streamPreviewInfo).appendTo(channelItem);
         }
+
         $(channelItem).appendTo("#channels");
+        $('[data-toggle="tooltip"]').tooltip(); // enable tooltips by hovering over info icon.
+
       });
     });
 
   }
 
 
-  var usernames = ["freecodecamp", "magic", "celinalin", "nanonoko","wsopreplaystream","jonathanlittle", "liveatthebike", "esl_sc2", "ogamingsc2", "habathcx", "terakilobyte", "thomasballinger", "comster404", "brunofin", "karltowns32"];
-  //coymeetsworld, somerandompersonthatdoesnt exist <-- how to handle madeup names
+  //var usernames = ["freecodecamp", "magic", "celinalin", "nanonoko","wsopreplaystream","jonathanlittle", "liveatthebike", "esl_sc2", "ogamingsc2", "habathcx", "terakilobyte", "thomasballinger", "comster404", "brunofin", "karltowns32", "somejunkuserthatdoesntexist"];
+  var usernames = ["freecodecamp", "magic", "celinalin", "nanonoko","wsopreplaystream","jonathanlittle", "liveatthebike", "esl_sc2", "ogamingsc2", "habathcx", "terakilobyte", "thomasballinger", "comster404", "brunofin", "karltowns32", "somejunkuserthatdoesntexist"];
   for (var i = 0; i < usernames.length; i++) {
     getUsers(usernames[i]);
   }
+
 });
