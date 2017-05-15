5f29383dd39239d8548a6dffaf14bdde274beceb
Coy Sanders <coymeetsworld@gmail.com>
2017-05-14 20:00:52 -0700

    Now renders account closed users correctly.

--- js/twitch.js
+++ js/twitch.js
@@ -3,8 +3,7 @@ $(document).ready(function() {
   const NO_USER_ICON_URL= "imgs/twitchDefaultIcon.png";
 
   /* List of users to query. */
-  const USERNAMES = ["freecodecamp", "magic", "channelfireball", "liveatthebike", "karltowns32", "celinalin", "nanonoko","wsopreplaystream","jonathanlittle", "esl_sc2", "ogamingsc2", "habathcx", "terakilobyte", "thomasballinger", "comster404", "brunofin", "somejunkuserthatdoesntexist"];
-  //const USERNAMES = ["comster404", "brunofin", "somejunkuserthatdoesntexist"];
+  const USERNAMES = ["freecodecamp", "yoda", "magic", "channelfireball", "liveatthebike", "karltowns32", "celinalin", "nanonoko","wsopreplaystream","jonathanlittle", "esl_sc2", "ogamingsc2", "habathcx", "terakilobyte", "thomasballinger", "comster404", "brunofin" ];
   //const USERNAMES = ["brunofin", "comster404"];
 
   const CLIENT_ID = "skji05ppnsavrfz5ydkkttvbbzj2h29";
@@ -142,9 +141,111 @@ console.log("X is "+x+" and Y is "+y);
 
 
       }
-      USERNAMES.map((user) => getUserInfo(user)); // So API call isn't made until featured streams finishes. This is done to prevent a duplicate channel from showing up (i.e. if one of the predefined channels ends up being featured at the time.)
+      //USERNAMES.map((user) => getUserInfo(user)); // So API call isn't made until featured streams finishes. This is done to prevent a duplicate channel from showing up (i.e. if one of the predefined channels ends up being featured at the time.)
+      USERNAMES.map((user) => getUserInfo2(user)); // So API call isn't made until featured streams finishes. This is done to prevent a duplicate channel from showing up (i.e. if one of the predefined channels ends up being featured at the time.)
     });
   }
+
+
+  const renderStreamData = (streamData, userData, channelItem) => {
+    console.log("Stream data:");
+    console.log(streamData);
+    let statusColumn = $('<div>');
+        statusColumn.attr('class', 'stream-status');
+
+        if (streamData.stream === null) {
+          channelItem.addClass('channel-offline');
+          statusColumn.text('Offline');
+          $(statusColumn).appendTo(channelItem);
+        } else {
+          channelItem.addClass('channel-online');
+          statusColumn.text(streamData.stream.channel.game + ": " + streamData.stream.channel.status);
+          $(statusColumn).appendTo(channelItem);
+          let streamPreview = $('<div>');
+          $(createPreviewImage(userData.name, streamData.stream.preview.large)).appendTo(streamPreview);
+          streamPreview.addClass('stream-preview');
+          $(streamPreview).appendTo(channelItem);
+          let streamPreviewInfo = $('<div>');
+          streamPreviewInfo.addClass('stream-preview-info');
+          streamPreviewInfo.html("Viewers: " + streamData.stream.viewers);
+          $(streamPreviewInfo).appendTo(channelItem);
+        }
+
+        $(channelItem).appendTo("#channels");
+
+  }
+
+  const renderUserData = (userName, userData) => {
+
+
+      if (featuredUsernames.includes(userName)) {
+        console.log("User already in featured users.");
+        return;
+      }
+
+      let channelItem = $('<li>');
+      channelItem.attr('class', 'flex-item');
+
+      let iconColumn = $('<div>');
+      iconColumn.attr('class', 'streamer-icon');
+      $(createUserImage(userData.logo)).appendTo(iconColumn);
+      $(iconColumn).appendTo(channelItem);
+
+      let nameColumn = $('<div>');
+      nameColumn.attr('class', 'streamer-name');
+      //$(createLink(userData.display_name, false)).appendTo(nameColumn);
+      $(createLink(userName, false)).appendTo(nameColumn);
+
+      if (userData.bio && userData.bio !== '') {
+        let span = $("<span>");
+        span.attr('tooltip', userData.bio);
+        span.attr("tooltip-position", "top");
+        let img = $('<img>');
+        img.attr('src', 'imgs/info.png');
+        img.appendTo(span);
+        span.appendTo(nameColumn);
+      }
+
+      $(nameColumn).appendTo(channelItem);
+
+      if (userData.hasOwnProperty("status") && userData.status == 422) {
+         console.log("Found an error: " + userData.error); 
+          let statusColumn = $('<div>'); //repeated code in renderStreamData, should refactor
+          statusColumn.attr('class', 'stream-status');
+
+          channelItem.addClass('channel-closed');
+          statusColumn.text('Account closed');
+          $(statusColumn).appendTo(channelItem);
+          $(channelItem).appendTo("#channels");
+          return;
+      }
+
+    $.ajax({
+      type: "GET",
+      url: 'https://api.twitch.tv/kraken/streams/'+userData.name,
+      headers: {
+        'CLIENT-ID': 'skji05ppnsavrfz5ydkkttvbbzj2h29'
+      },
+      success: function(data, textStatus, jqXHR ){
+         console.log("Success getting stream");
+         console.log(data);
+         console.log("Status: " + textStatus);
+         console.log("jqXHR: " + jqXHR);
+         renderStreamData(data, userData, channelItem);
+      
+      },
+      error: function(jqXHR, textStatus, errorThrown ) {
+         console.log("ErrorThrown getting stream:");
+         console.log(errorThrown);
+         console.log("Status: " + textStatus);
+         console.log(jqXHR);
+         console.log(jqXHR.responseJSON);
+         console.log(jqXHR.responseJSON.status);
+      }
+    });
+
+  }
+
 
   const getUserInfo2 = (user) => {
     console.log("Getting user info for " + user);
@@ -159,19 +260,23 @@ console.log("X is "+x+" and Y is "+y);
       },
       success: function(data, textStatus, jqXHR ){
          console.log("Success");
-         console.log("Data:");
          console.log(data);
          console.log("Status: " + textStatus);
          console.log("jqXHR: " + jqXHR);
+         renderUserData(user, data);
+
+
+
       },
       error: function(jqXHR, textStatus, errorThrown ) {
-         console.log("Error");
          console.log("ErrorThrown:");
          console.log(errorThrown);
          console.log("Status: " + textStatus);
          console.log(jqXHR);
          console.log(jqXHR.responseJSON);
          console.log(jqXHR.responseJSON.status);
+         //if (jqXHR.responseJSON.status == 422)
+         renderUserData(user, jqXHR.responseJSON);
       }
     });
 
@@ -191,8 +296,6 @@ console.log("X is "+x+" and Y is "+y);
         console.log("User already in featured users.");
         return;
       }
-
-console.log("Adding " + userData.name);
 
       let channelItem = $('<li>');
       channelItem.attr('class', 'flex-item');
